5650faca0fcca562bb05b9bfad20e94dee4f42b0
Mohammad <mam29740@gmail.com>
2026-05-16 21:14:00 +0300

    Start Asynchronous Queues

--- app/cart/views.py
+++ app/cart/views.py
@@ -31,3 +31,4 @@ class CartDeleteView(DestroyAPIView):
     # إذا كان هناك عدة منتجات، سيرمي خطأ MultipleObjectsReturned
     def get_object(self):
         return Cart.objects.filter(user=self.request.user).first() 
+    print("ma7wa")
