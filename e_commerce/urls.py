99eea319e79691f4e5f2484af11676c849ff0f4b
juba <mohmad287.ali.alali@gmail.com>
2026-05-17 22:55:52 +0300

    fix pug where get from sonarcloud.io secound round

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from django_psutil_dash.urls import psutil_urlpatterns  # ← إضافة
+from django_psutil_dash.urls import psutil_urlpatterns 
 
-# router = DefaultRouter()
-# # router.register(...)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(router.urls)),
     path('api/accounts/', include('app.accounts.urls')),
     path('api/products/', include('app.product.urls')),
     path('api/cart/', include('app.cart.urls')),
@@ -17,9 +14,8 @@ urlpatterns = [
     path('api/wallets/', include('app.wallets.urls')),
     
 
-    path('silk/', include('silk.urls', namespace='silk')),  # ← تفاصيل كل طلب
-    path('system/', psutil_urlpatterns()),                  # ← موارد النظام الحية
-
+    path('silk/', include('silk.urls', namespace='silk')),  
+    path('system/', psutil_urlpatterns()),                 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
